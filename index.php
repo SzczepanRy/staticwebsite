47690ebcda644d18cf0ce4b0559f707dfbfc0a0a
szczepan <109472029+SzczepanRy@users.noreply.github.com>
2024-12-12 11:23:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>szczepan</title>
+<link rel="stylesheet" href="style.css">
+<style>
+	*{
+	padding:0;
+	margin:0;
+	}
+	body{
+	width:100%;
+	height:100%;
+	background:url("./aaa.jpg", 100% ,100%);
+	display:flex;
+	align-items:center;
+	justify-contnent:center;
+	}
+</style>
+</head>
+<body>
+	<h1> szczepan Rydzewski super Static GALLERIAA </h1>
+	<ul>
+		<li/> <img src="./OIP.jpg" /> </li>
+		<li/> <img src="./pobierz.jpg" /> </li>
+	</ul> 
+</body>
+</html>
